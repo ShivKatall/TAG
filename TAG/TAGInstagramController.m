a1080af089ca11658e76165c3a43fcc22ed27d71
Cole Bratcher <colebratcher@gmail.com>
2014-08-12 10:56:21 -0700

    Added 'createTokenRequestWithURL', 'postTokenDataTaskWithRequest' and 'createTokenFromResponseData' methods to 'InstagramController'

--- TAG/TAGInstagramController.m
+++ TAG/TAGInstagramController.m
@@ -38,4 +38,51 @@
     return [components lastObject];
 }
 
+-(NSMutableURLRequest *)createTokenRequestWithURL:(NSURL *)url;
+{
+    NSString *code = [self getCodeFromCallbackURL:url];
+    NSString *grantType = @"authorization_code";
+    NSString *parameterString = [NSString stringWithFormat:@"client_id=%@&client_secret=%@&grant_type=%@&redirect_uri=%@&code=%@",INSTAGRAM_CLIENT_ID, INSTAGRAM_CLIENT_SECRET, grantType, INSTAGRAM_CALLBACK_URI, code];
+    NSData *postData = [parameterString dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+    NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long) [postData length]];
+    
+    NSMutableURLRequest *tokenRequest = [NSMutableURLRequest new];
+    [tokenRequest setURL:[NSURL URLWithString:@"https://api.instagram.com/oauth/access_token"]];
+    [tokenRequest setHTTPMethod:@"POST"];
+    [tokenRequest setValue:postLength forHTTPHeaderField:@"context-length"];
+    [tokenRequest setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"content-type"];
+    [tokenRequest setHTTPBody:postData];
+    
+    return tokenRequest;
+}
+
+-(void)postTokenDataTaskWithRequest:(NSMutableURLRequest *)request
+{
+    NSURLSessionDataTask *tokenDataTask = [self.URLSession dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        
+        if (error) {
+            NSLog(@"Error: %@", error.description);
+        }
+        
+        NSLog(@"%@", response.description);
+        NSString *token = [self createTokenFromResponseData:data];
+        [[NSUserDefaults standardUserDefaults] setValue:token forKey:@"oauthtoken"];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+    }];
+    
+    [tokenDataTask resume];
+}
+
+-(NSString *)createTokenFromResponseData:(NSData *)data
+{
+    NSString *tokenResponse     = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
+    NSArray *tokenComponents    = [tokenResponse componentsSeparatedByString:@"&"];
+    NSString *tokenWithCode     = tokenComponents[0];
+    NSArray *tokenArray         = [tokenWithCode componentsSeparatedByString:@"="];
+    
+    NSLog(@"%@", tokenArray);
+    
+    return tokenArray[1];
+}
+
 @end
